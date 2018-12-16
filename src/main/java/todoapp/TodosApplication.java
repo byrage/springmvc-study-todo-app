b498a20dcc89130f67e3fbd1f776aa0115d4d656
byrage <byrage91@gmail.com>
2018-12-16 18:44:57 +0900

    Extract to CsvView

--- src/main/java/todoapp/TodosApplication.java
+++ src/main/java/todoapp/TodosApplication.java
@@ -1,30 +1,64 @@
 package todoapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+
+import todoapp.commons.web.view.CsvView;
 
 @SpringBootApplication
 public class TodosApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(TodosApplication.class, args);
     }
 
     @Bean
     public WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
-            // GET /assets/css/todos.css
 
-            /* @Override
+            @Override
             public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("/assets/**")
-                        .addResourceLocations("assets");
-            } */
+                // HTTP GET -> /assets/css/todos.css
+
+    /*
+    registry.addResourceHandler("/assets/**")
+         .addResourceLocations("assets/", "file:./files/assets/", "classpath:assets/");
+    */
+
+                // aws s3 리소스를 획득에서 제공해줘
+                // s3:bucketName/path/
+            }
+
+            @Override
+            public void configureViewResolvers(ViewResolverRegistry registry) {
+                // registry.viewResolver(new SimpleMappingViewResolver());
+
+                // Spring MVC로 ContentNegotiation을 구성 할 때
+                // registry.enableContentNegotiation(defaultViews);
+            }
+
         };
     }
 
-}
+    /*
+        TIP : Spring Boot에 구성된 ContentNegotiation 전략을 바꾸고 싶을 때
+    */
+    @Autowired
+    public void configContentNegotiation(ContentNegotiatingViewResolver viewResolver) {
+        List<View> defaultViews = new ArrayList<>(viewResolver.getDefaultViews());
+        defaultViews.add(new CsvView());
 
+        viewResolver.setDefaultViews(defaultViews);
+    }
+
+}
--- src/main/java/todoapp/web/todos/TodoController.java
+++ src/main/java/todoapp/web/todos/TodoController.java
@@ -1,8 +1,15 @@
 package todoapp.web.todos;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
@@ -10,51 +17,54 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.AbstractView;
+
+import todoapp.commons.domain.Spreadsheet;
 import todoapp.core.todos.application.TodoFinder;
 import todoapp.core.todos.domain.Todo;
 import todoapp.web.model.SiteProperties;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.*;
-
 @Controller
 public class TodoController {
 
-    /*    private Environment env;
-
-        public TodoController(Environment env) {
-
-            this.env = env;
-        }
-    */
-    private SiteProperties siteProperties;
     private TodoFinder todoFinder;
-
-    public TodoController(SiteProperties siteProperties, TodoFinder todoFinder) {
-
-        this.siteProperties = siteProperties;
+    private SiteProperties siteProperties;
+    public TodoController(TodoFinder todoFinder, SiteProperties siteProperties) {
         this.todoFinder = todoFinder;
+        this.siteProperties = siteProperties;
     }
 
     @ModelAttribute("site")
     public SiteProperties getSiteProperties() {
-
         return siteProperties;
     }
 
-    /*
-             1. ModelAndView (default)
-             2. return String + Model params
-             3. return void + Model params (CoC)
-            */
-    @RequestMapping("/todos")
-    public void todos(Model model) {
+    // viewName: todos
+    // classpath:/tem../todos.html
 
-        model.addAttribute("todos", todoFinder.getAll());
+    @RequestMapping("/todos")
+    public void todos() {
+
     }
 
-    @Component("todos")
+    @RequestMapping(value = "/todos", produces = "text/csv")
+    public Spreadsheet downloadTodos() {
+        List<Todo> todos = todoFinder.getAll();
+
+        Spreadsheet.Row header = new Spreadsheet.Row()
+                .addCell("id")
+                .addCell("title")
+                .addCell("complated");
+        List<Spreadsheet.Row> rows = todos.stream().map(todo -> new Spreadsheet.Row()
+                .addCell(todo.getId())
+                .addCell(todo.getTitle())
+                .addCell(todo.isCompleted()))
+                                          .collect(Collectors.toList());
+
+        return new Spreadsheet("todos", header, rows);
+    }
+
+
+    // @Component("todos")
     public static class TodoCsvView extends AbstractView {
 
         private final Logger log = LoggerFactory.getLogger(this.getClass());
@@ -65,28 +75,30 @@ public class TodoController {
 
         @Override
         protected boolean generatesDownloadContent() {
-
             return true;
         }
 
         @Override
-        protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-            log.debug("CSV 컨텐츠를 출력합니다.");
+        protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
+                                               HttpServletResponse response) throws Exception {
+            log.debug("TodoCsvView: CSV 컨텐츠를 출력합니다.");
 
             String fileName = "attachment; filename=\"todos.csv\"";
             response.setHeader(HttpHeaders.CONTENT_DISPOSITION, fileName);
 
-            String header = "id,title,completed";
+            String header = "id,titile,complated";
             response.getWriter().println(header);
 
+            // 모델로부터 할 일 목록을 추출해서, CSV 방식으로 출력하기
             List<Todo> todos = (List<Todo>) model.getOrDefault("todos", Collections.emptyList());
             for (Todo todo : todos) {
-                response.getWriter().println(String.format("%d,%s,%s",
-                                                           todo.getId(), todo.getTitle(), todo.isCompleted()));
+                String line = String.format("%d,%s,%s", todo.getId(), todo.getTitle(), todo.isCompleted());
+                response.getWriter().println(line);
             }
 
             response.flushBuffer();
         }
+
     }
+
 }
