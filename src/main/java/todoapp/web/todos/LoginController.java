a94025a9abbe72e00ca9a3458eaec54e43c7d0b2
byrage <byrage91@gmail.com>
2018-12-17 10:42:52 +0900

    Add feature related login

--- src/main/java/todoapp/web/todos/LoginController.java
+++ src/main/java/todoapp/web/todos/LoginController.java
@@ -2,23 +2,30 @@ package todoapp.web.todos;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.*;
-import todoapp.core.todos.application.TodoEditor;
-import todoapp.core.todos.application.TodoFinder;
-import todoapp.core.todos.domain.Todo;
-import todoapp.core.user.domain.User;
-import todoapp.web.model.FeatureTogglesProperties;
+import todoapp.core.user.application.UserJoinder;
+import todoapp.core.user.application.UserPasswordVerifier;
+import todoapp.core.user.domain.UserEntityNotFoundException;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Size;
-import java.util.List;
 
 @Controller
 public class LoginController {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    private UserPasswordVerifier userPasswordVerifier;
+    private UserJoinder userJoinder;
+
+    public LoginController(UserPasswordVerifier userPasswordVerifier, UserJoinder userJoinder) {
+
+        this.userPasswordVerifier = userPasswordVerifier;
+        this.userJoinder = userJoinder;
+    }
 
     @GetMapping("/login")
     public String loginForm() {
@@ -27,9 +34,38 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public void login(@Valid LoginCommand loginCommand) {
+    public String login(@Valid LoginCommand loginCommand) {
 
-        log.debug("loginCommand={}", loginCommand);
+        log.debug("loginCommand={}, bindingResult={}", loginCommand);
+
+        /*
+        수동으로 스프링으로부터 에러를 가져와서 처리하는 방식
+        bindingResult를 파라미터로 받을 경우 스프링에서 따로 후처리를 안해줌
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("bindingResult", bindingResult);
+            model.addAttribute("message", "login error");
+            return "login";
+        }
+        */
+
+        try {
+            userPasswordVerifier.verify(loginCommand.getUsername(), loginCommand.getPassword());
+        } catch (UserEntityNotFoundException notFoundException) {
+            userJoinder.join(loginCommand.getUsername(), loginCommand.getPassword());
+        }
+
+        return "redirect:/todos";
+    }
+
+    // LoginController 스코프에만 유효한 방식
+    // 전체 컨트롤러에 적용할때는 에러처리 클래스 생성후 @ControllerAdvice 어노테이션 추가
+    @ExceptionHandler(BindException.class)
+    public String handleBindException(BindException error, Model model) {
+        model.addAttribute("bindingResult", error.getBindingResult());
+        model.addAttribute("message", "사용자 이름 또는 비밀번호가 값이 올바르지 않습니다.");
+
+        return "login";
     }
 
     public static class LoginCommand {
